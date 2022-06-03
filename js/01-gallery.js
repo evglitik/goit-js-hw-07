e2f4ebcf47fdcba24fd2aa75cfdf773fcf2a69ea
Yevhen Lytvynchuk <95911893+evglitik@users.noreply.github.com>
2022-06-03 13:34:49 +0300

    create lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const containerGalery = document.querySelector(".gallery");
+
+renderGalery(galleryItems);
+
+function renderGalery(images) {
+  const newGalery = images
+    .map(
+      (el) =>
+        `<a class="gallery__link" href="${el.original}">
+        <img
+        class="gallery__image"
+        src="${el.preview}"
+        data-source="${el.original}"
+        alt="${el.description}"/>
+    </a>`
+    )
+    .join("");
+  containerGalery.insertAdjacentHTML("beforeend", newGalery);
+}
+
+containerGalery.addEventListener("click", openOriginalImag);
+
+function openOriginalImag(ev) {
+  ev.preventDefault();
+
+  const instance = basicLightbox.create(`
+      <img src=${ev.target.dataset.source} width="800" height="600">
+  `);
+
+  instance.show();
+
+  document.addEventListener("keydown", cancelKeyboard)
+
+  function cancelKeyboard(e) {
+      if (e.key === "Escape") {
+        instance.close();
+        document.removeEventListener("keydown", cancelKeyboard);
+      }
+    }
+}
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+/*
+[Створення і рендер розмітки на підставі масиву даних galleryItems 
+і наданого шаблону елемента галереї.Використовуй готовий код з першого завдання.]
+
+Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs. 
+Необхідно додати посилання на два файли: simple - lightbox.min.js і simple - lightbox.min.css.
+
+Ініціалізація бібліотеки після створення і додання елементів галереї у
+div.gallery.Для цього ознайомся з документацією SimpleLightbox - насамперед секції
+«Usage» і «Markup».
+
+Подивися в документації секцію «Options» і додай відображення підписів 
+до зображень з атрибута alt.Нехай підпис буде знизу і з'являється через 
+250 мілісекунд після відкриття зображення.
+*/
+
+const containerGalery = document.querySelector(".gallery");
+
+renderGalery(galleryItems);
+
+function renderGalery(images) {
+  const newGalery = images
+    .map(
+      (el) =>
+        `<li><a class="gallery__item" href="${el.original}">
+            <img class="gallery__image" src="${el.preview}" alt="${el.description}"/>
+        </a></li>`
+    )
+    .join("");
+  containerGalery.insertAdjacentHTML("beforeend", newGalery);
+}
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  overlayOpacity: 0.5,
+  captionsData: "alt",
+  captionDelay: 250,
+});
